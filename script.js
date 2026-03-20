6ac8abad0349e577775c93e19daf158d9895f18e
RATCHA <ratchatvm@gmail.com>
2026-03-20 15:17:05 +0530

    updated project

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function openLogin(){
-document.getElementById("loginPopup").style.display="flex";
+function openLogin() {
+    document.getElementById("loginPopup").style.display = "flex";
 }
 
-function closeLogin(){
-document.getElementById("loginPopup").style.display="none";
+function closeLogin() {
+    document.getElementById("loginPopup").style.display = "none";
 }
 
-function bookTrip(event){
-event.preventDefault();
-document.getElementById("msg").innerText="Trip booked successfully!";
+function loginUser() {
+    document.getElementById("loginPopup").style.display = "none";
+    showPopup("Login Successful ✅");
+}
+
+function bookTrip(event) {
+    event.preventDefault();
+    showPopup("Booking Confirmed 🎉");
+}
+
+function showPopup(message) {
+    document.getElementById("popupMessage").innerHTML = message;
+    document.getElementById("successPopup").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("successPopup").style.display = "none";
 }
